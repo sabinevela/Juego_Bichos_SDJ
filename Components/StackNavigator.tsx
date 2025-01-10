85201f1b2fb22051c8fb55cd1a3be7155b66a93b
sabinevela <lukehemmingsteamomucho2005@gmail.com>
2025-01-10 18:32:04 -0500

    Correccion

--- Components/StackNavigator.tsx
+++ Components/StackNavigator.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Welcome from '../Screens/Welcome';
-import LoginScreen from '../Screens/InicioSesion';
+import LoginScreen from '../Screens/InicioSesion'; 
 import RegisterScreen from '../Screens/Registro';
 import Aplicacion from '../Screens/Aplicacion';
-import Puntaje from '../Screens/Playerrr';
+import Puntaje from '../Screens/Playerrr';  
 import PaginaPrincipal from '../Screens/PaginaPrincipal';
 import GitHub from '../Screens/GitHub';
 import Restablecer from '../Screens/Recuperacion';
@@ -17,17 +17,17 @@ export default function AppNavegador() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="PaginaPrincipal">
-      <Stack.Screen name="PaginaPrincipal" component={PaginaPrincipal} />
+        <Stack.Screen name="PaginaPrincipal" component={PaginaPrincipal} />
         <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Log" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} /> 
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Aplicacion" component={Aplicacion} />
-        <Stack.Screen name="Playerrr" component={Puntaje} /> 
+        <Stack.Screen name="Puntaje" component={Puntaje} /> 
         <Stack.Screen name="GitHub" component={GitHub}/>
         <Stack.Screen name="Perfil" component={Perfil} />
         <Stack.Screen name="Restaurar" component={Restablecer} />
-        <Stack.Screen name="Perfil" component={Perfil} /> 
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
